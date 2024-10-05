92049fb616e5ae396d504e85c9f85399a3da5a52
akshay <akrajguru111@gmail.com>
2024-10-05 12:41:22 +0530

     Implement a token-based Authentication.  Develop APIs for each operation.  Include CURD (ADD, UPDATE, DELETE) operation.

---
+++ EmployeeManagementSystem/EmsServices/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using EmsServices.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EmsServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly List<Users> usersData = new List<Users>()
+        {
+            new Users{ Id= 1, Username="test", Password="test"},
+            new Users{ Id=2, Username="new", Password="new"}
+        };
+        IConfiguration _config;
+        public AccountController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login([FromBody] UserLogin request)
+        {
+            var user = Authenticate(request);
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+            return NotFound("User not found");
+        }
+        private string Generate(Users user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Name, user.Username),
+
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private Users Authenticate(UserLogin userLogin)
+        {
+            Users results = null;
+            if (usersData.Any(x => x.Username == userLogin.Username && x.Password == userLogin.Password))
+            {
+                results = usersData.First(z => z.Username == userLogin.Username && z.Password == userLogin.Password);
+            }
+
+
+            if (results != null)
+            {
+                return results;
+            }
+            return null;
+        }
+    }
+}
---
+++ EmployeeManagementSystem/EmsServices/Controllers/EmployeeController.cs
@@ -0,0 +1,83 @@
+﻿using EmsServices.Model;
+using EmsServices.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmsServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployee _employee;
+        private readonly ILogger<EmployeeController> _logger;
+        public EmployeeController(IEmployee employee, ILogger<EmployeeController> logger)
+        {
+            _employee = employee ??
+           throw new ArgumentNullException(nameof(employee));
+            _logger = logger;
+        }
+
+
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _employee.GetEmployees());
+        }
+        //Get details for a specific Users
+        [HttpGet]
+        [Route("GetEmployeeByID/{ID}")]
+        public async Task<IActionResult> GetEmployeeByID(int ID)
+        {
+            var result = await _employee.GetEmployeeByID(ID);
+            if (result is null)
+            {
+                return NotFound();
+            }
+            else
+                return Ok(result);
+        }
+
+        //Add a new Users
+        [HttpPost]
+        [Route("AddUsers")]
+        public async Task<IActionResult> Post(Employee employee)
+        {
+            var result = await _employee.AddEmployee(employee);
+            if (!(result is null))
+            {
+                return Ok("Added Successfully");
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Something Went Wrong");
+            }
+
+        }
+        //Update details for a Users
+        [HttpPut]
+        [Route("UpdateUsers")]
+        public async Task<IActionResult> Put(Employee Users)
+        {
+            await _employee.UpdateEmployee(Users);
+            return Ok("Updated Successfully");
+        }
+        //Delete a Users
+        [HttpDelete]
+        [Route("DeleteEmployee")]
+        public IActionResult Delete(int id)
+        {
+            var result = _employee.DeleteEmployee(id);
+            if (result)
+                return NoContent();
+            else
+                return NotFound();
+        }
+    }
+}
