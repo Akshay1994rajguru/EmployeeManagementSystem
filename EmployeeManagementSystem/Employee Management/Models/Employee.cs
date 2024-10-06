0befd68b4e1a3024aedbaf475cacfe18d314f659
Akshay1994rajguru <156409094+akshayrajguru111@users.noreply.github.com>
2024-10-06 23:35:51 +0530

    UI updated

---
+++ EmployeeManagementSystem/Employee Management/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Employee_Management.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+
+        public string LastName { get; set; }
+
+        public string Position { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
